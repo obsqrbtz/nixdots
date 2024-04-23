0ff82db17d2aa6f1731ce3a75c9c66dab8bea5c1
Daniel Dada <doesdeos@gmail.com>
2024-04-24 01:48:32 +0300

    removed ozone env

--- nixos/modules/env.nix
+++ nixos/modules/env.nix
@@ -1,7 +1,6 @@
 {
   environment.variables = {
     EDITOR = "vim";
-    NIXOS_OZONE_WL = "1";
     #RANGER_LOAD_DEFAULT_RC = "FALSE";
     #QT_QPA_PLATFORMTHEME = "qt5ct";
     #GSETTINGS_BACKEND = "keyfile";
