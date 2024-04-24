cc4fd5c43aba0f94706f7e937eb6c9a1379b0c5a
Daniel Dada <doesdeos@gmail.com>
2024-04-24 22:36:10 +0300

    mako config

--- home-manager/modules/mako.nix
+++ home-manager/modules/mako.nix
@@ -1,6 +1,17 @@
 {
   programs.mako = {
     enable = true;
+    layer = "overlay";
+    font = "JetBrainsMono 13";
+    width = 300;
+    height = 80;
     defaultTimeout = 4000;
+    maxVisible = 10;
+    margin = "25,25,25,25";
+    borderRadius = 10;
+    borderSize = 3;
+    backgroundColor = "#121212";
+    textColor = "#c2c2b0";
+    borderColor = "#5f8787";
   };
 }
