0ba7245c3c2840365750546414c32fff0e4c79cd
Daniel Dada <doesdeos@gmail.com>
2024-04-26 22:14:34 +0300

    nixvim plugins

--- nixos/modules/nixvim/nixvim.nix
+++ nixos/modules/nixvim/nixvim.nix
@@ -3,21 +3,25 @@
   programs.nixvim = {
     enable = true;
     defaultEditor = true;
-    plugins.lightline.enable = true;
-    plugins.coq-nvim.enable = true;
-    plugins.lsp = {
-      enable = true;
-      servers = {
-        nixd.enable = true;
-        bashls.enable = true;
-        clangd.enable = true;
-        rust-analyzer.enable = true;
-        csharp-ls.enable = true;
+    plugins = {
+      lightline.enable = true;
+      cmp.enable = true;
+      telescope.enable = true;
+      treesitter.enable = true;
+      lsp = {
+        enable = true;
+        servers = {
+          nixd.enable = true;
+          bashls.enable = true;
+          clangd.enable = true;
+          rust-analyzer.enable = true;
+          csharp-ls.enable = true;
+        };
       };
-    };
-    plugins.nvim-cmp = {
-      enable = true;
-      completion.autocomplete = [ "TextChanged" ];
+      nvim-cmp = {
+        enable = true;
+        completion.autocomplete = [ "TextChanged" ];
+      };
     };
   };
 }
