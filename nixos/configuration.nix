c55b8119f4cba0e204c828756010d7dcb2f5ad87
Daniel Dada <doesdeos@gmail.com>
2024-04-26 20:16:13 +0300

    set up dualboot

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -8,8 +8,18 @@
       ./modules/bundle.nix
     ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  #boot.loader.systemd-boot.enable = true;
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+    grub = {
+      enable = true;
+      devices = [ "nodev" ];
+      efiSupport = true;
+      useOSProber = true;
+    };
+  };
+
+  time.hardwareClockInLocalTime = true;
 
   networking.hostName = "nixos";
 
