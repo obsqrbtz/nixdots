fa1ffe0391fb48eb034eebadf1cceb66f66f0ec4
Daniel Dada <doesdeos@gmail.com>
2025-12-12 03:04:31 +0300

    updated dev shells

--- dev-shells/flake.nix
+++ dev-shells/flake.nix
@@ -3,11 +3,13 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-    in {
+    in
+    {
       devShells.${system} = {
         js = pkgs.mkShell {
           buildInputs = with pkgs; [
@@ -20,19 +22,73 @@
         };
 
         rust = pkgs.mkShell {
-          buildInputs = with pkgs; [ rustc cargo rustfmt clippy rust-analyzer ];
+          buildInputs = with pkgs; [
+            rustc
+            cargo
+            rustfmt
+            clippy
+            rust-analyzer
+          ];
         };
 
         go = pkgs.mkShell {
-          buildInputs = with pkgs; [ go gopls gotools go-tools ];
+          buildInputs = with pkgs; [
+            go
+            gopls
+            gotools
+            go-tools
+          ];
         };
 
         cpp = pkgs.mkShell {
-          buildInputs = with pkgs; [ gcc clang-tools cmake gnumake gdb ];
+          buildInputs = with pkgs; [
+            gcc
+            clang-tools
+            cmake
+            gnumake
+            gdb
+          ];
+        };
+
+        kaskad = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gcc
+            stdenv.cc.cc.lib
+            ninja
+            cmake
+            swig
+            dotnet-sdk_10
+
+            qt6.qtbase
+            qt6.qttools
+            qt6.qtsvg
+            qt6.qtserialport
+            qt6.wrapQtAppsHook
+
+            clang-tools
+            gdb
+
+            pkg-config
+          ];
+
+          shellHook = ''
+                                    export CMAKE_GENERATOR="Ninja"
+                                    export CMAKE_EXPORT_COMPILE_COMMANDS=ON
+                                    echo "KaskadCPP development environment loaded"
+                                    echo "Compiler: $(gcc --version | head -n1)"
+                                    echo "CMake: $(cmake --version | head -n1)"
+                                    echo "Ninja: $(ninja --version)"
+                                    echo "Qt6: $(qmake6 -v | grep 'Qt version')"
+                                    echo "SWIG: $(swig -version | grep 'SWIG Version')"
+            			echo ".NET: $(dotnet --version)"
+          '';
         };
 
         csharp = pkgs.mkShell {
-          buildInputs = with pkgs; [ dotnet-sdk_10 omnisharp-roslyn ];
+          buildInputs = with pkgs; [
+            dotnet-sdk_10
+            omnisharp-roslyn
+          ];
         };
       };
     };
